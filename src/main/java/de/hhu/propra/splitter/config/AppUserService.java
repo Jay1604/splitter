1b82a8ba67d6e6b2302abd8a0663aea7f1d0546d
Teserakt27 <92543231+Teserakt27@users.noreply.github.com>
2023-02-28 12:18:14 +0100

    add Basic SecurityConfig and Github OAuth

---
+++ src/main/java/de/hhu/propra/splitter/config/AppUserService.java
@@ -0,0 +1,26 @@
+package de.hhu.propra.splitter.config;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppUserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+
+  private final DefaultOAuth2UserService defaultService = new DefaultOAuth2UserService();
+
+  @Override
+  public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+    System.out.println("User Service called"); // TODO: Delete
+    OAuth2User originalUser = defaultService.loadUser(userRequest);
+    Set<GrantedAuthority> authorities = new HashSet<>(originalUser.getAuthorities());
+    return new DefaultOAuth2User(authorities, originalUser.getAttributes(), "id");
+  }
+}
---
+++ src/main/java/de/hhu/propra/splitter/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package de.hhu.propra.splitter.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+  @Bean
+  public SecurityFilterChain configure(HttpSecurity chainBuilder) throws Exception {
+    chainBuilder
+        .authorizeHttpRequests(
+            configurer -> configurer
+                .antMatchers("/public", "/css/*", "/favicon.ico")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+        )
+        .oauth2Login(config ->
+            config.userInfoEndpoint(
+                info -> info.userService(new AppUserService())
+            ));
+
+    return chainBuilder.build();
+  }
+
+}
