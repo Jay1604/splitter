65a5746fa846068de4ec38bc0b095634fa5bbb41
Jay1604 <janniklas.niebisch@gmail.com>
2023-03-01 12:57:10 +0100

    add Gruppenübersicht mit Thymeleaf-Integration

--- src/main/java/de/hhu/propra/splitter/web/WebController.java
+++ src/main/java/de/hhu/propra/splitter/web/WebController.java
@@ -1,9 +1,13 @@
 package de.hhu.propra.splitter.web;
 
+import static java.util.function.Predicate.not;
+
 import de.hhu.propra.splitter.domain.model.Gruppe;
+import de.hhu.propra.splitter.domain.model.Person;
 import de.hhu.propra.splitter.services.GruppenService;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.Set;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
@@ -20,15 +24,24 @@ public class WebController {
   @Autowired
   public WebController(final GruppenService gruppenService) {
     this.gruppenService = gruppenService;
+    Gruppe gruppe1 = new Gruppe(1L, new Person("..."), "Ausflug");
+    Gruppe gruppe2 = new Gruppe(2L, new Person("..."), "Kein Ausflug mehr");
+    gruppe2.setIstOffen(false);
+    gruppenService.addGruppe(gruppe1);
+    gruppenService.addGruppe(gruppe2);
   }
 
   @GetMapping("/")
   public String index(Model m, OAuth2AuthenticationToken auth) {
-    Set<Gruppe> geschlosseneGruppen = gruppenService.getGruppenForGithubName(
+    Set<Gruppe> gruppen = gruppenService.getGruppenForGithubName(
         auth.getPrincipal().getAttribute("login"));
 
-    System.out.println(geschlosseneGruppen);
+    var geschlosseneGruppen = gruppen.stream().filter(not(Gruppe::isIstOffen))
+        .collect(Collectors.toSet());
+    var offeneGruppen = gruppen.stream().filter(Gruppe::isIstOffen).collect(Collectors.toSet());
 
+    m.addAttribute("offeneGruppen", offeneGruppen);
+    m.addAttribute("geschlosseneGruppen", geschlosseneGruppen);
     return "index";
   }
 
