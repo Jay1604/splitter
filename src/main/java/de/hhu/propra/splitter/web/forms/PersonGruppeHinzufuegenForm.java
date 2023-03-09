583bb0d67cf2c9f135ab9d083eb461fdcd4a2049
ThiloSavaryHHU <Jan.Savary@uni-duesseldorf.de>
2023-03-09 10:12:00 +0100

    Add validator for GitHub username

--- src/main/java/de/hhu/propra/splitter/web/forms/PersonGruppeHinzufuegenForm.java
+++ src/main/java/de/hhu/propra/splitter/web/forms/PersonGruppeHinzufuegenForm.java
@@ -2,7 +2,15 @@ package de.hhu.propra.splitter.web.forms;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
-public record PersonGruppeHinzufuegenForm(@NotBlank String name, @NotNull Long id) {
+public record PersonGruppeHinzufuegenForm(
+    @NotBlank
+    @Pattern(
+        regexp = "^[a-zA-Z\\d](?:[a-zA-Z\\d]|-(?=[a-zA-Z\\d])){0,38}$",
+        message = "Muss valider GitHub Name sein"
+    )
+    String name,
+    @NotNull Long id) {
 
 }
--- src/main/java/de/hhu/propra/splitter/web/rest/objects/SimpleGruppenEntity.java
+++ src/main/java/de/hhu/propra/splitter/web/rest/objects/SimpleGruppenEntity.java
@@ -5,8 +5,15 @@ import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.List;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 @SuppressFBWarnings({"EI_EXPOSE_REP", "EI_EXPOSE_REP2"})
 public record SimpleGruppenEntity(String gruppe,
                                   @NotBlank String name,
-                                  @NotEmpty List<String> personen) {}
+                                  @NotEmpty List<@Pattern(
+                                      regexp = "^[a-zA-Z\\d](?:[a-zA-Z\\d]"
+                                          + "|-(?=[a-zA-Z\\d])){0,38}$",
+                                      message = "Muss valider GitHub Name sein"
+                                  ) String> personen) {
+
+}
